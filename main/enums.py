d292e4933ca5f48531ea2f136225ea3272de3e7d
grace-pham <gracepham208@gmail.com>
2021-06-24 00:55:08 +0700

    fix: Add categories to ExpenseCategory enum

--- main/enums.py
+++ main/enums.py
@@ -4,12 +4,15 @@ from enum import Enum
 class EnumBase(Enum):
     @classmethod
     def get_list(cls):
-        attributes = [member.value for role, member in cls.__members__.items()]
+        attributes = [att_value.value for att, att_value in cls.__members__.items()]
         return attributes
 
 
 class ExpenseCategory(EnumBase):
+    HEALTHCARE = "Healthcare"
     UTILITIES = "Utilities"
     RENT = "Rent"
+    FOOD_AND_BEVERAGE = "Food & Beverage"
+    NECESSITIES = "Necessities"
     TRAVEL = "Travel"
     ENTERTAINING = "Entertaining"
